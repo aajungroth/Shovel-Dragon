34bdaa1136b64f42d2b5c934c78c9584ddb04187
aajungroth <aajungroth@ucdavis.edu>
2022-06-16 11:13:36 -0700

    Add level reader method to track reader script

--- Assets/Scripts/Model/TrackReader.cs
+++ Assets/Scripts/Model/TrackReader.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class TrackReader : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    public IDictionary<string, bool> readLevel(string levelText) {
+      IDictionary<string, bool> level = new Dictionary<string, bool>();
+
+      return level;
     }
 }
