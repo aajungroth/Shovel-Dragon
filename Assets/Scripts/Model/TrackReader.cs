038b0aaeb1293041b8f238b2892350bb20deff3f
aajungroth <aajungroth@ucdavis.edu>
2022-06-16 11:31:16 -0700

    Add basic track reader method and add comments to the track reader script

--- Assets/Scripts/Model/TrackReader.cs
+++ Assets/Scripts/Model/TrackReader.cs
@@ -14,6 +14,15 @@ public class TrackReader : MonoBehaviour
         
     }
 
+    // Each track is a list of levels that can be progressed through
+    public List<IDictionary<string, bool>> readTrack(List<string> trackText) {
+      List<IDictionary<string, bool>> track = new List<IDictionary<string, bool>>();
+
+      return track;
+    }
+
+    // Each level is represented as a dictionary with coordinates as keys and
+    // booleans as values to represent squares that can be walked on
     private IDictionary<string, bool> readLevel(string levelText) {
       IDictionary<string, bool> level = new Dictionary<string, bool>();
 
