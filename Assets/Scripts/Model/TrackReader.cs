4189b627324dbd1c56baa01755e6b2dcced45286
aajungroth <aajungroth@ucdavis.edu>
2022-06-16 11:49:57 -0700

    Add foreach loop to read track method in track reader script

--- Assets/Scripts/Model/TrackReader.cs
+++ Assets/Scripts/Model/TrackReader.cs
@@ -4,28 +4,32 @@ using UnityEngine;
 
 public class TrackReader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start() {
-        
+  // Start is called before the first frame update
+  void Start() {
+      
+  }
+
+  // Update is called once per frame
+  void Update() {
+      
+  }
+
+  // Each track is a list of levels that can be progressed through
+  public List<IDictionary<string, bool>> readTrack(List<string> trackText) {
+    List<IDictionary<string, bool>> track = new List<IDictionary<string, bool>>();
+
+    foreach(string levelText in trackText) {
+      track.Add(readLevel(levelText));
     }
 
-    // Update is called once per frame
-    void Update() {
-        
-    }
+    return track;
+  }
 
-    // Each track is a list of levels that can be progressed through
-    public List<IDictionary<string, bool>> readTrack(List<string> trackText) {
-      List<IDictionary<string, bool>> track = new List<IDictionary<string, bool>>();
+  // Each level is represented as a dictionary with coordinates as keys and
+  // booleans as values to represent squares that can be walked on
+  private IDictionary<string, bool> readLevel(string levelText) {
+    IDictionary<string, bool> level = new Dictionary<string, bool>();
 
-      return track;
-    }
-
-    // Each level is represented as a dictionary with coordinates as keys and
-    // booleans as values to represent squares that can be walked on
-    private IDictionary<string, bool> readLevel(string levelText) {
-      IDictionary<string, bool> level = new Dictionary<string, bool>();
-
-      return level;
-    }
+    return level;
+  }
 }
