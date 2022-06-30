12f0d5f15650f63e8dee02af7b544ff2163483ae
aajungroth <aajungroth@ucdavis.edu>
2022-06-30 10:19:41 -0700

    Add state variables, add reference to TrackReader script, write start method

--- Assets/Scripts/Model/TrackModel.cs
+++ Assets/Scripts/Model/TrackModel.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackModel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class TrackModel : MonoBehaviour {
+  // List of level text files
+  public List<string> trackText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // List of objects in the game that the player interacts with
+  // that are not tiles or decorations
+  public List<GameObject> interactableList;
+
+  // Reads in a list of levels
+  private TrackReader trackReader;
+
+  // The list of levels as matrices that tracks empty and blocked tiles
+  private List<IDictionary<string, bool>> track;
+
+  // Start is called before the first frame update
+  void Start() {
+    trackReader = gameObject.GetComponent<TrackReader>();
+    track = trackReader.readTrack(trackText);
+  }
+
+  // Update is called once per frame
+  void Update() {
+    
+  }
 }
