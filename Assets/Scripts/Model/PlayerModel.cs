149fa20ce538f2ec55ace8d5d3f355110504422b
aajungroth <aajungroth@ucdavis.edu>
2022-07-12 10:49:24 -0700

    Add variable to track the intial player level

--- Assets/Scripts/Model/PlayerModel.cs
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerModel : MonoBehaviour {
+    public int initialPlayerLevel;
     public List<Vector2> initialPlayerPositionList;
 }
