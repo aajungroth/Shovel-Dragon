48d8cb1fcc6b1c725db7470f5343fbac490a6a6c
aajungroth <aajungroth@ucdavis.edu>
2022-07-12 10:51:29 -0700

    Add initial player direction with a default value of right

--- Assets/Scripts/Model/PlayerModel.cs
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerModel : MonoBehaviour {
+    public string intialPlayerDirection = "right";
     public int initialPlayerLevel;
     public List<Vector2> initialPlayerPositionList;
 }
