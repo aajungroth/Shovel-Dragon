536678be38127ddc2a68f36483dcde52179d4840
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 15:48:19 -0700

    Update GridMovement to handle game objects, add assembly reference to System, add action argument

--- Assets/Scripts/Player Controls/GridMovement.cs
+++ Assets/Scripts/Player Controls/GridMovement.cs
@@ -1,45 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour {
-  private void validateMoveUp() {
-    if (!isMoving) {
-      StartCoroutine(MovePlayer(Vector3.up));
-    }
+  public void MoveGameObjectUp() {
+    StartCoroutine(MoveGameObject(Vector3.up));
   }
 
-  private void validateMoveRight() {
-    if (!isMoving) {
-      StartCoroutine(MovePlayer(Vector3.right));
-    }
+  public void MoveGameObjectRight() {
+    StartCoroutine(MoveGameObject(Vector3.right));
   }
 
-  private void validateMoveDown() {
-    if (!isMoving) {
-      StartCoroutine(MovePlayer(Vector3.down));
-    }
+  public void MoveGameObjectDown() {
+    StartCoroutine(MoveGameObject(Vector3.down));
   }
 
-  private void validateMoveLeft() {
-   if (!isMoving) {
-      StartCoroutine(MovePlayer(Vector3.left));
-    } 
+  public void MoveGameObjectLeft() {
+    StartCoroutine(MoveGameObject(Vector3.left));
   }
 
-  private IEnumerator MovePlayer(Vector3 direction) {
-    Vector3 positionOriginal = transform.position;
+  private IEnumerator MoveGameObject(Action done, GameObject targetGameObject, Vector3 direction) {
+    Vector3 positionOriginal = targetGameObject.transform.position;
     Vector3 positionTarget = positionOriginal + direction;
     float timeElapsed = 0;
 
     while (timeElapsed < timeToMove) {
-      transform.position = Vector3.Lerp(positionOriginal, positionTarget, timeElapsed / timeToMove);
+      targetGameObject.transform.position = Vector3
+        .Lerp(positionOriginal, positionTarget, timeElapsed / timeToMove);
       timeElapsed += Time.deltaTime;
       yield return null;
     }
 
     transform.position = positionTarget;
 
-    // add reference to function in level controller
+    done();
   }
 }
