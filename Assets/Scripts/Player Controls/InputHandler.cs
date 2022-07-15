73a22a4f83ec5bbd1c974b2069032c65ab278cb1
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 15:23:14 -0700

    Move handling input from the grid movement to the input handler

--- Assets/Scripts/Player Controls/InputHandler.cs
+++ Assets/Scripts/Player Controls/InputHandler.cs
@@ -2,17 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class InputHandler : MonoBehaviour {
+  // The buttons used to control movement
+  public Button buttonUp;
+  public Button buttonRight;
+  public Button buttonDown;
+  public Button buttonLeft;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  // Only one button input will be processed at a time
+  public bool isInputEnabled = true;
+
+  // Start is called before the first frame update
+  void Start() {
+    buttonUp.onClick.AddListener(validateMoveUp);
+    buttonRight.onClick.AddListener(validateMoveRight);
+    buttonDown.onClick.AddListener(validateMoveDown);
+    buttonLeft.onClick.AddListener(validateMoveLeft);
+  }
+
+  private void requestMoveUp() {
+    if (isInputEnabled) {
+      isInputEnabled = false;
+      // level controller function
     }
+  }
+
+  private void requestMoveRight() {
+    if (isInputEnabled) {
+      isInputEnabled = false;
+      // level controller function
+    }
+  }
+
+  private void requestMoveDown() {
+    if (isInputEnabled) {
+      isInputEnabled = false;
+      // level controller function
+    }
+  }
+
+  private void requestMoveLeft() {
+   if (isInputEnabled) {
+      isInputEnabled = false;
+      // level controller function
+    } 
+  }
 }
