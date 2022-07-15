06870527605154cc2355cae314e75422ef9950a2
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 16:01:40 -0700

    Add comments

--- Assets/Scripts/Player Controls/GridMovement.cs
+++ Assets/Scripts/Player Controls/GridMovement.cs
@@ -4,22 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour {
+  // Starts the coroutine to move a GameObject up
   public void MoveGameObjectUp(Action done, GameObject targetGameObject, float timeToMove) {
     StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.up));
   }
 
+  // Starts the coroutine to move a GameObject right
   public void MoveGameObjectRight(Action done, GameObject targetGameObject, float timeToMove) {
     StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.right));
   }
 
+  // Starts the coroutine to move a GameObject down
   public void MoveGameObjectDown(Action done, GameObject targetGameObject, float timeToMove) {
     StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.down));
   }
 
+  // Starts the coroutine to move a GameObject left
   public void MoveGameObjectLeft(Action done, GameObject targetGameObject, float timeToMove) {
     StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.left));
   }
 
+  // Moves a GameObject in the requested time and diretion and signals when the movement is complete
   private IEnumerator MoveGameObject(Action done, GameObject targetGameObject, float timeToMove, Vector3 direction) {
     Vector3 positionOriginal = targetGameObject.transform.position;
     Vector3 positionTarget = positionOriginal + direction;
