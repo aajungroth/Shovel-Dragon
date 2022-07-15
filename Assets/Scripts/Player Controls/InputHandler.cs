5ff54458ebe02515fc950592b64c05d880593749
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 15:26:17 -0700

    Move reference to unity UI to input handler

--- Assets/Scripts/Player Controls/InputHandler.cs
+++ Assets/Scripts/Player Controls/InputHandler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputHandler : MonoBehaviour {
   // The buttons used to control movement
