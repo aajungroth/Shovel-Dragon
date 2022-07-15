98b5e8afc73dfa054734676a426b63be8eb73007
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 16:51:21 -0700

    Adjust spacing

--- Assets/Scripts/Scene Change/ChangeScene.cs
+++ Assets/Scripts/Scene Change/ChangeScene.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
-{
-    public void MoveToScene(int sceneId)
-    {
-      SceneManager.LoadScene(sceneId);
-    }
+public class ChangeScene : MonoBehaviour {
+  public void MoveToScene(int sceneId) {
+    SceneManager.LoadScene(sceneId);
+  }
 }
