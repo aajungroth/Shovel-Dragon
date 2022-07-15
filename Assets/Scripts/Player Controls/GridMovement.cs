4d5cf8c82a5e7aa1e2b5df70f4c6af4db9450bfa
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 15:58:35 -0700

    Add timeToMove parameter to methods and fix typo

--- Assets/Scripts/Player Controls/GridMovement.cs
+++ Assets/Scripts/Player Controls/GridMovement.cs
@@ -4,23 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour {
-  public void MoveGameObjectUp(Action done, GameObject targetGameObject) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, Vector3.up));
+  public void MoveGameObjectUp(Action done, GameObject targetGameObject, float timeToMove) {
+    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.up));
   }
 
-  public void MoveGameObjectRight(Action done, GameObject targetGameObject) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, Vector3.right));
+  public void MoveGameObjectRight(Action done, GameObject targetGameObject, float timeToMove) {
+    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.right));
   }
 
-  public void MoveGameObjectDown(Action done, GameObject targetGameObject) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, Vector3.down));
+  public void MoveGameObjectDown(Action done, GameObject targetGameObject, float timeToMove) {
+    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.down));
   }
 
-  public void MoveGameObjectLeft(Action done, GameObject targetGameObject) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, Vector3.left));
+  public void MoveGameObjectLeft(Action done, GameObject targetGameObject, float timeToMove) {
+    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.left));
   }
 
-  private IEnumerator MoveGameObject(Action done, GameObject targetGameObject, Vector3 direction) {
+  private IEnumerator MoveGameObject(Action done, GameObject targetGameObject, float timeToMove, Vector3 direction) {
     Vector3 positionOriginal = targetGameObject.transform.position;
     Vector3 positionTarget = positionOriginal + direction;
     float timeElapsed = 0;
