f1854179dc8b7a3ac90463c9065a60c28161f6f9
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 18:03:30 -0700

    Update GridMovement to work with transforms instead of GameObjects

--- Assets/Scripts/Controller/GridMovement.cs
+++ Assets/Scripts/Controller/GridMovement.cs
@@ -4,42 +4,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour {
-  // Starts the coroutine to move a GameObject down
-  public void MoveGameObjectDown(Action done, GameObject targetGameObject, float timeToMove) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.down));
+  // Starts the coroutine to move a transform down
+  public void MoveTransformDown(Action done, Transform targetTransform, float timeToMove) {
+    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.down));
   }
 
-  // Starts the coroutine to move a GameObject left
-  public void MoveGameObjectLeft(Action done, GameObject targetGameObject, float timeToMove) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.left));
+  // Starts the coroutine to move a transform left
+  public void MoveTransformLeft(Action done, Transform targetTransform, float timeToMove) {
+    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.left));
   }
 
-  // Starts the coroutine to move a GameObject right
-  public void MoveGameObjectRight(Action done, GameObject targetGameObject, float timeToMove) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.right));
+  // Starts the coroutine to move a transform right
+  public void MoveTransformRight(Action done, Transform targetTransform, float timeToMove) {
+    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.right));
   }
 
-  // Starts the coroutine to move a GameObject up
-  public void MoveGameObjectUp(Action done, GameObject targetGameObject, float timeToMove) {
-    StartCoroutine(MoveGameObject(done, targetGameObject, timeToMove, Vector3.up));
+  // Starts the coroutine to move a transform up
+  public void MoveTransformUp(Action done, Transform targetTransform, float timeToMove) {
+    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.up));
   }
 
-  // Moves a GameObject in the requested time and diretion and signals when the movement is complete
-  private IEnumerator MoveGameObject(Action done, GameObject targetGameObject, float timeToMove, Vector3 direction) {
-    Vector3 originalPosition = targetGameObject.transform.position;
+  // Moves a transform in the requested time and diretion and signals when the movement is complete
+  private IEnumerator MoveTransform(Action done, Transform targetTransform, float timeToMove, Vector3 direction) {
+    Vector3 originalPosition = targetTransform.position;
     Vector3 targetPosition = originalPosition + direction;
     float timeElapsed = 0;
 
     // This performs the movement across multiple frames
     while (timeElapsed < timeToMove) {
-      targetGameObject.transform.position = Vector3
+      targetTransform.position = Vector3
         .Lerp(originalPosition, targetPosition, timeElapsed / timeToMove);
       timeElapsed += Time.deltaTime;
       yield return null;
     }
 
     // Ensures the Game Object reaches the target position exactly
-    targetGameObject.transform.position = targetPosition;
+    targetTransform.position = targetPosition;
 
     // Signals to the original caller that the coroutine is finished
     done();
