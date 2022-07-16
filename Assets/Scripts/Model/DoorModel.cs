aec1d654d2e72a69958187c7a961b969eee66224
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 19:05:07 -0700

    Add public data members to DoorModel

--- Assets/Scripts/Model/DoorModel.cs
+++ Assets/Scripts/Model/DoorModel.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorModel : MonoBehaviour {
+  // The starting position of the door
+  public Vector2 initialPosition;
 
+  // Tests if door has been unlocked
+  public bool isLocked = true;
 }
