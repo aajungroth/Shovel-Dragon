2103a392eadb131be2ae15f7c26937978a6ebe3c
aajungroth <aajungroth@ucdavis.edu>
2022-07-16 12:17:44 -0700

    Add code to find the player from the entity list, add a method to get the player, add player variable

--- Assets/Scripts/Model/EntityManager.cs
+++ Assets/Scripts/Model/EntityManager.cs
@@ -6,21 +6,30 @@ public class EntityManager : MonoBehaviour {
   // A list of all GameObjects that are entities in the game
   private List<GameObject> entityList;
 
+  // The GameObject that represents the player
+  private GameObject player;
+
   // Awake is called before any other Start method
   void Awake() {
     entityList = GetChildren();
+    player = FindPlayer(entityList);
   }
 
   // Gets the list of entities
-  public GetList() {
+  public List<GameObject> GetList() {
     return entityList;
+  }
+
+  // Gets the player
+  public GameObject GetPlayer() {
+    return player;
   }
 
   // Gets the list of GameObjects that are entities 
   private List<GameObject> GetChildren() {
     List<GameObject> entityList = new List<GameObject>();
     Transform[] transformList = GetComponentsInChildren<Transform>(); 
-    
+
     foreach (Transform childTransform in transformList) {
       GameObject childGameObject = childTransform.gameObject;
 
@@ -31,5 +40,20 @@ public class EntityManager : MonoBehaviour {
     }
 
     return entityList;
+  }
+
+  // Gets the player GameObjects from a list entity GameObjects
+  private GameObject FindPlayer(List<GameObject> entityList) {
+    GameObject player = new GameObject();
+
+    foreach (GameObject entity in entityList) {
+      // Tests if the GameObject is a player
+      if (entity.GetComponent<PlayerModel>() != null) {
+        player = entity;
+        break;
+      }
+    }
+
+    return player;
   }
 }
