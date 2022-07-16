0a254ef270a88e99e58d4403b714444f4ef6aab8
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 19:56:00 -0700

    Add code to get the entities as list of GameObjects to the LevelController

--- Assets/Scripts/Controller/LevelController.cs
+++ Assets/Scripts/Controller/LevelController.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour {
+  // Manages all of the entities in the game
+  public EntityManager entityManager;
+
+  // A list of all GameObjects that are entities in the game
+  private List<GameObject> entityList;
+
   // Start is called before the first frame update
   void Start() {
-    
+    // Get the entities as a list of GameObjects
+    entityList = entityManager.GetList();
   }
 
   // Determines if the player's attempted move will result in the
