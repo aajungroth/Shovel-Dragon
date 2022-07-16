9dd2c3dc891c9038e4ae328ff0d1f7814e55081c
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 18:59:02 -0700

    Update comments

--- Assets/Scripts/Model/MonsterModel.cs
+++ Assets/Scripts/Model/MonsterModel.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterModel : EntityModel {
-  // The starting position of the interactable
+  // The starting position of the monster
   public Vector2 initialPosition;
 
   // Tests if the monster has been buried
   public bool isBuried = false;
 
-  // The name of the interactable's AI script if it is a monster
+  // The name of the monster's AI script
   public string monsterAI;
 }
