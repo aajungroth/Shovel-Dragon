670e340f80a94ca0018f55636469e2cb8f6d001d
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 18:49:29 -0700

    Add public data members for MonsterModel

--- Assets/Scripts/Model/MonsterModel.cs
+++ Assets/Scripts/Model/MonsterModel.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterModel : EntityModel {
+  // The starting position of the interactable
+  public Vector2 initialPosition;
 
+  // Tests if the monster has been buried
+  public bool isBuried = false;
+
+  // The name of the interactable's AI script if it is a monster
+  public string monsterAI;
 }
