db0df3cdb4d1b0e965d0d9990f46485df3a0527e
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 19:27:55 -0700

    Remove unused methods and adjust spacing

--- Assets/Scripts/Model/HoleModel.cs
+++ Assets/Scripts/Model/HoleModel.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HoleModel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class HoleModel : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
