0d49068063cfda31731cd1f25d52a4a2549db078
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 19:24:35 -0700

    Add public data members to the PowerUpModel

--- Assets/Scripts/Model/PowerUpModel.cs
+++ Assets/Scripts/Model/PowerUpModel.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpModel : EntityModel {
+  // The starting position of the power up
+  public Vector2 initialPosition;
 
+  // Tests if the power up has been buried
+  public bool isBuried = true;
+
+  // The name of the power up
+  public string powerUpName;
 }
