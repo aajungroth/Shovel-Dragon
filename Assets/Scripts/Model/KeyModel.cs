769e7888f5227c891148b40718fa9e32fbaa38a0
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 18:58:22 -0700

    Add public data memebers to KeyModel

--- Assets/Scripts/Model/KeyModel.cs
+++ Assets/Scripts/Model/KeyModel.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyModel : EntityModel {
+  // The starting position of the key
+  public Vector2 initialPosition;
 
+  // Tests if the key has been buried
+  public bool isBuried = false;
 }
