19cd0706b44f474ec0418e6590e8a6629b5d6b92
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 17:11:15 -0700

    Update GetList to return a list of interactable GameObjects

--- Assets/Scripts/Model/InteractableManager.cs
+++ Assets/Scripts/Model/InteractableManager.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class InteractableManager : MonoBehaviour {
   // Gets the list of GameObjects that are interactables 
-  public List<Interactable> GetList() {
-    List<Interactable> interactableList = new List<Interactable>();
-    Interactable[] interactableComponentList = GetComponentsInChildren<Interactable>(); 
+  public List<GameObject> GetList() {
+    List<GameObject> interactableList = new List<GameObject>();
+    transform[] transformList = GetComponentsInChildren<transform>(); 
     
-    foreach (Interactable interactableComponent in interactableComponentList) {
-      interactableList.Add(interactableComponent);
+    foreach (transform childTransform in transformList) {
+      GameObject childGameObject = childTransform.gameObject;
+
+      // Tests if the GameObject is an interactable
+      if (childGameObject.GetComponent<InteractableModel>() != null) {
+        interactableList.Add(childGameObject);
+      }
     }
 
     return interactableList;
