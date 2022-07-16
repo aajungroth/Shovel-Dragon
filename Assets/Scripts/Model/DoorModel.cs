6b781fb2ec3b3a930fa3f1eeaa3cb132d82e7afd
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 19:03:37 -0700

    Remove unused methods and adjust spacing

--- Assets/Scripts/Model/DoorModel.cs
+++ Assets/Scripts/Model/DoorModel.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorModel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class DoorModel : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
