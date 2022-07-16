e0f321b6dc555bdfc8bd3aef17ea6ef6be256acd
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 18:56:37 -0700

    Remove unused variables and adjust spacing

--- Assets/Scripts/Model/KeyModel.cs
+++ Assets/Scripts/Model/KeyModel.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyModel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class KeyModel : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
