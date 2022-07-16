c204acc0b86c8e0b9f16ddf18215c79ad429d5bf
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 18:53:14 -0700

    Update all instances of interactable to entity

--- Assets/Scripts/Model/EntityManager.cs
+++ Assets/Scripts/Model/EntityManager.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableManager : MonoBehaviour {
-  // Gets the list of GameObjects that are interactables 
+public class EntityManager : MonoBehaviour {
+  // Gets the list of GameObjects that are entities 
   public List<GameObject> GetList() {
-    List<GameObject> interactableList = new List<GameObject>();
+    List<GameObject> entityList = new List<GameObject>();
     Transform[] transformList = GetComponentsInChildren<Transform>(); 
     
     foreach (Transform childTransform in transformList) {
       GameObject childGameObject = childTransform.gameObject;
 
-      // Tests if the GameObject is an interactable
-      if (childGameObject.GetComponent<InteractableModel>() != null) {
-        interactableList.Add(childGameObject);
+      // Tests if the GameObject is an entity
+      if (childGameObject.GetComponent<EntityModel>() != null) {
+        entityList.Add(childGameObject);
       }
     }
 
-    return interactableList;
+    return entityList;
   }
 }
