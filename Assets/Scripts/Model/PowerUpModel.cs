8b40bdbbd61b1879e80929adadf60670c55fd741
aajungroth <aajungroth@ucdavis.edu>
2022-07-15 19:21:17 -0700

    Remove unused methods and adjust spacing

--- Assets/Scripts/Model/PowerUpModel.cs
+++ Assets/Scripts/Model/PowerUpModel.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpModel : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PowerUpModel : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
