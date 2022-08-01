528140adaaaf683791b87857a99907cf4d90aee1
aajungroth <aajungroth@ucdavis.edu>
2022-08-01 15:46:06 -0700

    Add methods to handle player input for movement

--- Assets/Scripts/Controller/LevelController.cs
+++ Assets/Scripts/Controller/LevelController.cs
@@ -9,6 +9,31 @@ public class LevelController : MonoBehaviour {
   // Stores the layout of each level in the track
   public TrackModel trackModel;
 
+  private string actionMoveDown = "move down";
+  private string actionMoveLeft = "move left";
+  private string actionMoveRight = "move right";
+  private string actionMoveUp = "move up";
+
+  // Handles requests to move the player down
+  public void HandleMoveDown() {
+    ExecuteTurn(Vector2.down, actionMoveDown);
+  }
+
+  // Handles requests to move the player left
+  public void HandleMoveLeft() {
+    ExecuteTurn(Vector2.left, actionMoveLeft);
+  }
+
+  // Handles requests to move the player right
+  public void HandleMoveRight() {
+    ExecuteTurn(Vector2.right, actionMoveRight);
+  }
+
+  // Handles requests to move the player up
+  public void HandleMoveUp() {
+    ExecuteTurn(Vector2.up, actionMoveUp);
+  }
+
   // Determines if the player's attempted move will result in the
   // player moving to an empty space
   public void IsMoveValid(Vector2 move) {
