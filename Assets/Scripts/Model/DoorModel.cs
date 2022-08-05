40b3816c99ca1139bbcdb14c59983195bf076fae
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 14:57:21 -0700

    Remove intial position from the door model

--- Assets/Scripts/Model/DoorModel.cs
+++ Assets/Scripts/Model/DoorModel.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorModel : EntityModel {
-  // The starting position of the door
-  public Vector2 initialPosition;
-
   // Tests if door has been unlocked
   public bool isLocked = true;
 }
