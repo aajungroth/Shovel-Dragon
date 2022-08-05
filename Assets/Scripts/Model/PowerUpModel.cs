0dad5e2154c83ef23947785930627dbc01bb1eab
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 15:15:58 -0700

    Add variables to track if and what the power up is following

--- Assets/Scripts/Model/PowerUpModel.cs
+++ Assets/Scripts/Model/PowerUpModel.cs
@@ -6,6 +6,13 @@ public class PowerUpModel : EntityModel {
   // Tests if the power up has been buried
   public bool isBuried = true;
 
+  // Tests if the power up is following an entity
+  public bool isFollowing = false;
+
+  // The name of what the power up is following
+  // ie none, player, MonsterAI names
+  public string leader;
+
   // The name of the power up
   public string powerUpName;
 }
