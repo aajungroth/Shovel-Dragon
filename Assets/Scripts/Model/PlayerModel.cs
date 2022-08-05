e2c1f38636b7e4e1156d9820a502c62977bffe22
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 15:00:21 -0700

    Remove intialPositionList from the PlayerModel

--- Assets/Scripts/Model/PlayerModel.cs
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -2,7 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerModel : EntityModel {
-  // The list of initial positions the player takes in each level
-  public List<Vector2> initialPositionList;
-}
+public class PlayerModel : EntityModel {}
