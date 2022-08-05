8b85a00e6d2ab847bf7e3eb64349e3f59d238565
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 16:17:47 -0700

    Update tutorial controller class name

--- Assets/Scripts/Controller/TrackController/TutorialController.cs
+++ Assets/Scripts/Controller/TrackController/TutorialController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackControllerTutorial : TrackController
+public class TutorialController : TrackController
 {
 
 }
