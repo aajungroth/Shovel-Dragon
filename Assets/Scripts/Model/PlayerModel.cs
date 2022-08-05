82ac2b4d96dddb4727ea2bedac5c75704e18986d
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 15:20:22 -0700

    Add follower list to the PlayerModel

--- Assets/Scripts/Model/PlayerModel.cs
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -2,4 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerModel : EntityModel {}
+public class PlayerModel : EntityModel {
+  // The list of unique entities following the player
+  public List<string> followerList;
+}
