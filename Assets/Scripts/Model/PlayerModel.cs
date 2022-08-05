12469f04910f9382bf826ca3e0af964b86a974a3
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 14:36:26 -0700

    Remove direction variables and methods from the playerModel script

--- Assets/Scripts/Model/PlayerModel.cs
+++ Assets/Scripts/Model/PlayerModel.cs
@@ -3,27 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerModel : EntityModel {
-  // The public property for the private currentDirection variable
-  public string CurrentDirection {
-    get {
-      return currentDirection;
-    }
-    set {
-      currentDirection = value;
-    }
-  }
-
-  // The direction the player is facing when the game starts
-  public string initialDirection = "right";
-
   // The list of initial positions the player takes in each level
   public List<Vector2> initialPositionList;
-
-  // The direction the player is currently facing
-  private string currentDirection;
-
-  // Awake is called before any other Start method
-  void Awake() {
-    currentDirection = initialDirection;
-  }
 }
