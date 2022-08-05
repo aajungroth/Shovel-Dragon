5f827e4663a16714a0a923a423329d5cca237073
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 15:07:38 -0700

    Add variables to the key to track if and what it is following

--- Assets/Scripts/Model/KeyModel.cs
+++ Assets/Scripts/Model/KeyModel.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class KeyModel : EntityModel {
   // Tests if the key has been buried
   public bool isBuried = true;
+
+  // Tests if the key is following an entity
+  public bool isFollowing = false;
+
+  // The name of what the key is following
+  // ie none, player, or MonsterAI names
+  public string leader = none;
 }
