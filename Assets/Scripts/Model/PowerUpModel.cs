de6daa8482a0b41e728e796f6276e0b3cd46a131
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 14:57:59 -0700

    Remove initial position from the PowerUpModel

--- Assets/Scripts/Model/PowerUpModel.cs
+++ Assets/Scripts/Model/PowerUpModel.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpModel : EntityModel {
-  // The starting position of the power up
-  public Vector2 initialPosition;
-
   // Tests if the power up has been buried
   public bool isBuried = true;
 
