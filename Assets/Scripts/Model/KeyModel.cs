a6b8809ad182dc94e85e6330e7cadc8c6233caa2
aajungroth <aajungroth@ucdavis.edu>
2022-08-05 14:58:43 -0700

    Remove initial position from the KeyModel

--- Assets/Scripts/Model/KeyModel.cs
+++ Assets/Scripts/Model/KeyModel.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class KeyModel : EntityModel {
-  // The starting position of the key
-  public Vector2 initialPosition;
-
   // Tests if the key has been buried
   public bool isBuried = true;
 }
