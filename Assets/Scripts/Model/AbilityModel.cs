5625cf3b0d9fa3b631fab433106132dd0019e609
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 16:39:59 -0700

    Rename abilities

--- Assets/Scripts/Model/AbilityModel.cs
+++ Assets/Scripts/Model/AbilityModel.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class AbilityModel : MonoBehaviour {
   // Ability names to be used to differentiate entity abilities
-  public const string abilityMoveDown = "move down";
-  public const string abilityMoveLeft = "move left";
-  public const string abilityMoveRight = "move right";
-  public const string abilityMoveUp = "move up";
+  public const string moveDown = "move down";
+  public const string moveLeft = "move left";
+  public const string moveRight = "move right";
+  public const string moveUp = "move up";
 }
