879800de78645a5230bcfdb24ec8e6aed585bc22
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 16:31:50 -0700

    Add ability strings for movement

--- Assets/Scripts/Model/AbilityModel.cs
+++ Assets/Scripts/Model/AbilityModel.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AbilityModel : MonoBehaviour {
-
+  // Ability names to be used to differentiate entity abilities
+  public const string abilityMoveDown = "move down";
+  public const string abilityMoveLeft = "move left";
+  public const string abilityMoveRight = "move right";
+  public const string abilityMoveUp = "move up";
 }
