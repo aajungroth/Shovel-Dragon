a75b27877752ede01076339d805b5bb4c0ff2bde
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 16:51:24 -0700

    Add event limit set to 1000

--- Assets/Scripts/Controller/TrackController/TrackController.cs
+++ Assets/Scripts/Controller/TrackController/TrackController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrackController : MonoBehaviour {
+  // The maximum number of events that the TrackController will log
+  public int eventLimit = 1000;
+
   public void CollisionDetection() {
 
   }
