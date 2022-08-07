f508532369ea179425579927222a7a68beee6764
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 17:33:22 -0700

    Remove unused methods

--- Assets/Scripts/Controller/InvalidGridMovement.cs
+++ Assets/Scripts/Controller/InvalidGridMovement.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvalidGridMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class InvalidGridMovement : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
