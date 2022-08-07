5079c204ccdb65f491b235627807aa3d29b32478
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 17:00:09 -0700

    Make TutorialRoleManager a derived class of RoleManager

--- Assets/Scripts/Model/RoleManager/TutorialRoleManager.cs
+++ Assets/Scripts/Model/RoleManager/TutorialRoleManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialRoleManager : MonoBehaviour
+public class TutorialRoleManager : RoleManager
 {
 
 }
