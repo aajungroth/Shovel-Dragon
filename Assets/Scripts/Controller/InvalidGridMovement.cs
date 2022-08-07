c4bddae3abfc7298050c9a1e4af27120923cc09d
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 20:16:34 -0700

    Write stubs for InvalidGridMovement

--- Assets/Scripts/Controller/InvalidGridMovement.cs
+++ Assets/Scripts/Controller/InvalidGridMovement.cs
@@ -4,5 +4,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InvalidGridMovement : MonoBehaviour {
+  // Starts the coroutine to feint a transform down
+  public void FeintTransformDown(Action done, Transform targetTransform, float timeToMove) {
 
+  }
+
+  // Starts the coroutine to feint a transform left
+  public void FeintTransformLeft(Action done, Transform targetTransform, float timeToMove) {
+
+  }
+
+  // Starts the coroutine to feint a transform right
+  public void FeintTransformRight(Action done, Transform targetTransform, float timeToMove) {
+
+  }
+
+  // Starts the coroutine to feint a transform up
+  public void FeintTransformUp(Action done, Transform targetTransform, float timeToMove) {
+
+  }
+
+  // Feints a transform in the requested time and direction and signals when the movement is complete
+  private IEnumerator FeintTransform(Action done, Transform targetTransform, float timeToMove, Vector3 direction) {
+
+  }
 }
