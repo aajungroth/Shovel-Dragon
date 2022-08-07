c5528aecdea797415dce23cda8fd96e27387980a
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 20:20:02 -0700

    Write public handler methods

--- Assets/Scripts/Controller/InvalidGridMovement.cs
+++ Assets/Scripts/Controller/InvalidGridMovement.cs
@@ -6,22 +6,22 @@ using UnityEngine;
 public class InvalidGridMovement : MonoBehaviour {
   // Starts the coroutine to feint a transform down
   public void FeintTransformDown(Action done, Transform targetTransform, float timeToMove) {
-
+    StartCoroutine(FeintTransform(done, targetTransform, timeToMove, Vector3.down));
   }
 
   // Starts the coroutine to feint a transform left
   public void FeintTransformLeft(Action done, Transform targetTransform, float timeToMove) {
-
+    StartCoroutine(FeintTransform(done, targetTransform, timeToMove, Vector3.left));
   }
 
   // Starts the coroutine to feint a transform right
   public void FeintTransformRight(Action done, Transform targetTransform, float timeToMove) {
-
+    StartCoroutine(FeintTransform(done, targetTransform, timeToMove, Vector3.right));
   }
 
   // Starts the coroutine to feint a transform up
   public void FeintTransformUp(Action done, Transform targetTransform, float timeToMove) {
-
+    StartCoroutine(FeintTransform(done, targetTransform, timeToMove, Vector3.up));
   }
 
   // Feints a transform in the requested time and direction and signals when the movement is complete
