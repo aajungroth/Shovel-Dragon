85e3fe37815822bb5d88557d04c3019dc6f2deb6
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 17:31:21 -0700

    Write the TutorialRoleManager script

--- Assets/Scripts/Model/RoleManager/TutorialRoleManager.cs
+++ Assets/Scripts/Model/RoleManager/TutorialRoleManager.cs
@@ -2,7 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialRoleManager : RoleManager
-{
+public class TutorialRoleManager : RoleManager {
+  // The references to the GameObject entities that will be assigned the roles
+  public List<EntityModel> doorList;
+  public List<EntityModel> keyList;
+  public List<EntityModel> mapList;
 
+  public EntityModel emoteMonster;
+
+  // The names of the roles
+  public const string doorRole = "door";
+  public const string keyRole = "key";
+  public const string mapRole = "map";
+
+  public const string emoteMonsterRole = "emote";
+
+  // Awake is called before any other Start method
+  void Awake() {
+    AssignRoleList(doorList, doorRole);
+    AssignRoleList(keyList, keyRole);
+    AssignRoleList(mapList, mapRole);
+
+    AssignRole(emoteMonster, emoteMonsterRole);
+  }
 }
