e01f9746358d6de1301b3a91a1472e5e43ee8027
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 17:31:02 -0700

    Write RoleManager helper methods for subclasses

--- Assets/Scripts/Model/RoleManager/RoleManager.cs
+++ Assets/Scripts/Model/RoleManager/RoleManager.cs
@@ -2,4 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoleManager : MonoBehaviour {}
+public class RoleManager : MonoBehaviour {
+  protected void AssignRole(EntityModel entity, string role) {
+    entity.SetRole(role);
+  }
+
+  protected void AssignRoleList(List<EntityModel> entityList, string role) {
+    int count = 0;
+
+    foreach(EntityModel entity in entityList) {
+      entity.SetRole(role + count.ToString());
+      count++;
+    }
+  }
+}
