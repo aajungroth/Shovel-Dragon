63ade5e0e571e988b5fe13d14e4a0fbe4b7f075b
aajungroth <aajungroth@ucdavis.edu>
2022-08-07 16:29:14 -0700

    Remove unused methods

--- Assets/Scripts/MonsterAI/MonsterAIManager.cs
+++ Assets/Scripts/MonsterAI/MonsterAIManager.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterAIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class MonsterAIManager : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
