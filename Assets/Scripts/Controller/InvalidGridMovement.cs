8323890d9bbde4e962e0cbbd41229fc774e3a697
aajungroth <aajungroth@ucdavis.edu>
2022-08-06 20:39:06 -0700

    Write FeintTransform coroutine

--- Assets/Scripts/Controller/InvalidGridMovement.cs
+++ Assets/Scripts/Controller/InvalidGridMovement.cs
@@ -26,6 +26,42 @@ public class InvalidGridMovement : MonoBehaviour {
 
   // Feints a transform in the requested time and direction and signals when the movement is complete
   private IEnumerator FeintTransform(Action done, Transform targetTransform, float timeToMove, Vector3 direction) {
+    Vector3 originalPosition = targetTransform.position;
+    Vector3 targetPosition = originalPosition + direction;
+    float timeElapsed = 0;
 
+    bool firstHalfInProgress = true;
+
+    // This performs the first half of the feint across multiple frames
+    while (firstHalfInProgress && timeElapsed < timeToMove) {
+      targetTransform.position = Vector3
+        .Lerp(originalPosition, targetPosition, timeElapsed / timeToMove);
+      yield return null;
+    }
+
+    // Tests if the first first half of the feint completed
+    if (firstHalfInProgress && timeElapsed == timeToMove) {
+      // Allows the coroutine to progress to the next half of the feint
+      firstHalfInProgress = false;
+
+      // Ensures the GameObject reaches the target position exactly
+      targetTransform.position = targetPosition;
+
+      // Doubles the time to move to create time for the second half
+      timeToMove *= 2;
+    }
+
+    // This performs the second half of the fient across multiple frames
+    while (timeElapsed < timeToMove) {
+      targetTransform.position = Vector3
+        .Lerp(targetPosition, originalPosition, timeElapsed / timeToMove);
+      yield return null;
+    }
+
+    // Ensures the GameObject reaches the original position exactliy
+    targetTransform.position = originalPosition;
+
+    // Signals to the original caller that the coroutine is finished
+    done();
   }
 }
