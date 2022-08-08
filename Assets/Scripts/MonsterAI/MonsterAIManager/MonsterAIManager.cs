4a010d70fa95b886e48ddd7cba0b71fce39b4abf
aajungroth <aajungroth@ucdavis.edu>
2022-08-07 17:40:29 -0700

    Update targetPosition variable name to move

--- Assets/Scripts/MonsterAI/MonsterAIManager/MonsterAIManager.cs
+++ Assets/Scripts/MonsterAI/MonsterAIManager/MonsterAIManager.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class MonsterAIManager : MonoBehaviour {
   // Stores the Monster AI scripts by an AI name
   IDictionary<string, MonsterAI> monsterAIByName;
-  
+
   // Calls the Monster AI script to get the AI's next move
   public Vector2 GetMonsterMove(List<EntityModel> entityList,
   TrackModel trackModel, MonsterModel monster, string monsterAIName) {
-    Vector2 targetPosition = Vector2.zero;
+    Vector2 move = Vector2.zero;
 
     // Makes sure that the requested monster AI exists
     if (monsterAIByName.ContainsKey(monsterAIName)) {
       // Gets the monster AI script by name from the dictionary and calls
       // the GetMove method to get the next position to move to
-      targetPosition = monsterAIByName[monsterAIName]
+      move = monsterAIByName[monsterAIName]
         .GetMove(entityList, trackModel, monster);
     }
 
-    return targetPosition;
+    return move;
   }
 }
