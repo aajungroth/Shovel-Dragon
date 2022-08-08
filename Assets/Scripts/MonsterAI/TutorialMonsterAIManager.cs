53b85f6da33c080a8d5ae47c8a7e7dc1c8bf2b99
aajungroth <aajungroth@ucdavis.edu>
2022-08-07 17:07:52 -0700

    Write TutorialMonsterAIManager and set the class to be derived from MonsterAIManager

--- Assets/Scripts/MonsterAI/TutorialMonsterAIManager.cs
+++ Assets/Scripts/MonsterAI/TutorialMonsterAIManager.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialMonsterAIManager : MonoBehaviour {
+public class TutorialMonsterAIManager : MonsterAIManager {
+  // The AI script used for the track
+  public FixedMovementAI fixedMovementAI;
 
+  // The name of the AI scripts used for the dictionary key
+  public string fixedMovementAIName = "fixed movement";
+
+  // Awake is called before any other Start method
+  void Awake() {
+    // Add the AI script to the dictionary by name
+    monsterAIByName.Add(fixedMovementAIName, fixedMovementAI);
+  }
 }
