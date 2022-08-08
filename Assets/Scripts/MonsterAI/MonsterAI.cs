42fd5c2a69bc98a36dad48b24ee8e327368bca79
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 09:19:18 -0700

    Remove IsMoveValid

--- Assets/Scripts/MonsterAI/MonsterAI.cs
+++ Assets/Scripts/MonsterAI/MonsterAI.cs
@@ -3,18 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterAI : MonoBehaviour {
-  // Determines if a monster's attempted move will result in
-  // the monster moving to an empty space
-  protected bool IsMoveValid(TrackModel trackModel, MonsterModel monster, Vector2 move) {
-    List<IDictionary<string, bool>> track = trackModel.GetTrack();
-    int currentLevel = 0;
-    Vector2 targetPosition = Vector2.zero;
-    string targetPositionKey = "";
 
-    currentLevel = monster.GetCurrentLevel();
-    targetPosition = monster.GetCurrentPosition() + move;
-    targetPositionKey = targetPostion.x.ToString() + "," + targetPosition.y.ToString();
-
-    return track[currentLevel][targetPositionKey];
-  }
 }
