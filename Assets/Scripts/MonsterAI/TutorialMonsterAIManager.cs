4f178480b35e4aea98cbf8e78e781cc5513b94cf
aajungroth <aajungroth@ucdavis.edu>
2022-08-07 17:00:41 -0700

    Remove unused methods and adjust spacing

--- Assets/Scripts/MonsterAI/TutorialMonsterAIManager.cs
+++ Assets/Scripts/MonsterAI/TutorialMonsterAIManager.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialMonsterAIManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class TutorialMonsterAIManager : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
