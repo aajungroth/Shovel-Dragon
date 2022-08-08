0914f799438fe422c272593d2d7814aa3a665acf
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 15:33:36 -0700

    Write FixedMovementAI

--- Assets/Scripts/MonsterAI/FixedMovementAI.cs
+++ Assets/Scripts/MonsterAI/FixedMovementAI.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FixedMovementAI : MonsterAI {
+  public Vector2 direction = Vector2.right;
+
   public override Vector2 GetMove(List<EntityModel> entityList, TrackModel trackModel, EntityModel entity) {
-    return Vector2.zero;
+    // Tests if moving in the current direction would be invalid
+		if (!ValidateMovement.IsMoveValid(trackModel, entity, direction)) {
+			// Flips the direction
+      direction.x *= -1;
+			direction.y *= -1;
+		}
+
+    return direction;
 	}
 }
