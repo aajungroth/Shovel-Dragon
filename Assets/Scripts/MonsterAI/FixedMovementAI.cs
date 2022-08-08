d480c8110843c550689ed26a7367616529ae6957
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 10:28:44 -0700

    Add override of method GetMove

--- Assets/Scripts/MonsterAI/FixedMovementAI.cs
+++ Assets/Scripts/MonsterAI/FixedMovementAI.cs
@@ -3,5 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FixedMovementAI : MonsterAI {
-
+  public override Vector2 GetMove(List<EntityModel> entityList, TrackModel trackModel, EntityModel entity) {
+    return Vector2.zero;
+	}
 }
