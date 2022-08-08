20eae746a1e92d02471bb5f383288d974b79d70c
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 10:12:34 -0700

    Add parameters to the GetMove method

--- Assets/Scripts/MonsterAI/MonsterAI.cs
+++ Assets/Scripts/MonsterAI/MonsterAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MonsterAI : MonoBehaviour {
-  public Vector2 GetMove() {
+  public Vector2 GetMove(List<EntityModel> entityList, TrackModel trackModel, EntityModel entity) {
     return Vector2.zero;
   }
 }
