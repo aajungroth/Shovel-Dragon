19a54ea1f058133442aab2bcf0904070f0ff3804
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 18:33:55 -0700

    Refactor EntityAIManager to handle entities and return events

--- Assets/Scripts/EntityAI/EntityAIManager/EntityAIManager.cs
+++ Assets/Scripts/EntityAI/EntityAIManager/EntityAIManager.cs
@@ -3,30 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityAIManager : MonoBehaviour {
-  // Stores the Monster AI scripts by an AI name
-  protected IDictionary<string, MonsterAI> monsterAIByName;
+  // Stores the Entity AI scripts by an AI name
+  protected IDictionary<string, EntityAI> entityAIByName;
 
-  // Calls the Monster AI script to get the AI's next move
-  public Vector2 GetEntityMove(List<GameObject> entityList,
-  TrackModel trackModel, EntityModel entity) {
-    Vector2 move = Vector2.zero;
+  // Calls the Entity AI script to get the AI's next move
+  public EventModel GetEntityEvent(string ability, EntityModel entity,
+  List<GameObject> entityList, TrackModel trackModel) {
+    Vector2 currentPosition = entity.GetCurrentPosition();
+    Vector2 direction = Vector2.zero;
 
-    // Makes sure that the requested monster AI exists
-    if (monsterAIByName.ContainsKey(entity.AIName)) {
-      // Gets the monster AI script by name from the dictionary and calls
+    // Makes sure that the requested entity AI exists
+    if (entityAIByName.ContainsKey(entity.AIName)) {
+      // Gets the entity AI script by name from the dictionary and calls
       // the GetMove method to get the next position to move to
-      move = monsterAIByName[entity.AIName]
-        .GetMove(entityList, trackModel, entity);
+      direction = entityAIByName[entity.AIName]
+        .GetDirection(ability, entity, entityList, trackModel);
     }
 
     // Creates a new event that can be registered with the track controller
-    EventModel eventModel = new EventModel();
-    eventModel.abilityType = abilityType;
+    EventModel entityEvent = new EventModel();
+    eventModel.ability = ability;
     eventModel.direction = direction;
-    eventModel.endPosition = endPosition;
+    eventModel.endPosition = currentPosition + direction;
     eventModel.entity = entity;
-    eventModel.startPosition = startPosition;
+    eventModel.startPosition = currentPosition;
 
-    return move;
+    return entityEvent;
   }
 }
