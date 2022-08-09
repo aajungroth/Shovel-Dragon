e78b05413d8676ac0c9ff134d8b9ff06d3b2e7b4
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 18:50:08 -0700

    Update EntityType to GameObject

--- Assets/Scripts/EntityAI/EntityAI.cs
+++ Assets/Scripts/EntityAI/EntityAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityAI : MonoBehaviour {
-  public virtual Vector2 GetDirection(string ability, EntityModel entity,
+  public virtual Vector2 GetDirection(string ability, GameObject entity,
   List<GameObject> entityList, TrackModel trackModel) {
     return Vector2.zero;
   }
