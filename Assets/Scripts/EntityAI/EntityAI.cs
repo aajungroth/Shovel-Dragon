f7c7814d7def754f2d17a383c7fab0a04533271c
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 18:39:14 -0700

    Rename GetMove to GetDirection and adjust spacing

--- Assets/Scripts/EntityAI/EntityAI.cs
+++ Assets/Scripts/EntityAI/EntityAI.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EntityAI : MonoBehaviour {
-  public virtual Vector2 GetMove(string ability, EntityModel entity, List<GameObject> entityList, TrackModel trackModel) {
+  public virtual Vector2 GetDirection(string ability, EntityModel entity,
+  List<GameObject> entityList, TrackModel trackModel) {
     return Vector2.zero;
   }
 }
