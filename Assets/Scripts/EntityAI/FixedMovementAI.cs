8455ad11cd281235548662ce6445d259072baec7
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 18:43:58 -0700

    Update the list of entity models to be a list of GameObjects and adjust spacing

--- Assets/Scripts/EntityAI/FixedMovementAI.cs
+++ Assets/Scripts/EntityAI/FixedMovementAI.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class FixedMovementAI : EntityAI {
   public Vector2 direction = Vector2.right;
 
-  public override Vector2 GetDirection(string ability, EntityModel entity, List<EntityModel> entityList, TrackModel trackModel) {
+  public override Vector2 GetDirection(string ability, EntityModel entity,
+  List<GameObject> entityList, TrackModel trackModel) {
     // Tests if moving in the current direction would be invalid
 		if (!ValidateMovement.IsMoveValid(direction, entity, trackModel)) {
 			// Flips the direction
