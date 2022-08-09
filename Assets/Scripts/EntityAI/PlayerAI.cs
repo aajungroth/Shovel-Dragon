25c15c72e83b1e5767101bc68f0ff9f57ec99311
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 20:44:20 -0700

    Write PlayerAI

--- Assets/Scripts/EntityAI/PlayerAI.cs
+++ Assets/Scripts/EntityAI/PlayerAI.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAI : EntityAI {
+  public override EventModel GetEvent(GameObject entity, EventModel entityEvent,
+  List<GameObject> entityList, string playerAbility, Vector2 playerDirection,
+  TrackModel trackModel) {
+    entityEvent.ability = playerAbility;
+    entityEvent.direction = playerDirection;
+    entityEvent.endPosition = entityEvent.startPosition + playerDirection;
+    entityEvent.entity = entity;
 
+    return entityEvent;
+  }
 }
