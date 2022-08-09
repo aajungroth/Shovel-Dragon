4c635e0326c797972a5d366610597893f3fe3145
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 19:41:06 -0700

    Refactor GridMovement

--- Assets/Scripts/Controller/GridMovement.cs
+++ Assets/Scripts/Controller/GridMovement.cs
@@ -4,28 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridMovement : MonoBehaviour {
-  // Starts the coroutine to move a transform down
-  public void MoveTransformDown(Action done, Transform targetTransform, float timeToMove) {
-    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.down));
-  }
+  // Starts the coroutine to move a transform in a direction
+  public void MoveTransformInDirection(Action done, EventModel entityEvent) {
+    Transform targetTransform = entityEvent.entity.transform;
+    float timeToMove = entityEvent.entity.GetComponent<EntityModel>().timeToMove;
+    Vector3 direction = Vector3.zero;
 
-  // Starts the coroutine to move a transform left
-  public void MoveTransformLeft(Action done, Transform targetTransform, float timeToMove) {
-    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.left));
-  }
+    if (entityEvent.direction == Vector2.down) {
+      direction = Vector3.down;
+    }
+    else if (entityEvent.direction == Vector2.left) {
+      direction = Vector3.left;
+    }
+    else if (entityEvent.direction == Vector2.right) {
+      direction = Vector3.right;
+    }
+    else if (entityEvent.direction == Vector2.up) {
+      direction = Vector3.up;
+    }
 
-  // Starts the coroutine to move a transform right
-  public void MoveTransformRight(Action done, Transform targetTransform, float timeToMove) {
-    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.right));
-  }
-
-  // Starts the coroutine to move a transform up
-  public void MoveTransformUp(Action done, Transform targetTransform, float timeToMove) {
-    StartCoroutine(MoveTransform(done, targetTransform, timeToMove, Vector3.up));
+    StartCoroutine(MoveTransform(direction, done, targetTransform, timeToMove));
   }
 
   // Moves a transform in the requested time and direction and signals when the movement is complete
-  private IEnumerator MoveTransform(Action done, Transform targetTransform, float timeToMove, Vector3 direction) {
+  private IEnumerator MoveTransform(Vector3 direction, Action done, Transform targetTransform, float timeToMove) {
     Vector3 originalPosition = targetTransform.position;
     Vector3 targetPosition = originalPosition + direction;
     float timeElapsed = 0;
