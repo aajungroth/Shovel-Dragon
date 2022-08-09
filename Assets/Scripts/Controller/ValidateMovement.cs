225dce6094503259868959d893f3108a3aa8aa12
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 20:43:15 -0700

    Adjust spacing

--- Assets/Scripts/Controller/ValidateMovement.cs
+++ Assets/Scripts/Controller/ValidateMovement.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class ValidateMovement : MonoBehaviour {
   // Determines if the entity's attempted move will result in the
   // entity moving to an empty space
-  public static bool IsMoveValid(Vector2 direction, GameObject entity, TrackModel trackModel) {
+  public static bool IsMoveValid(Vector2 direction, GameObject entity,
+  TrackModel trackModel) {
     List<IDictionary<string, bool>> track = trackModel.GetTrack();
     int currentLevel = 0;
     Vector2 targetPosition = Vector2.zero;
     string targetPositionKey = "";
 
     currentLevel = entity.GetComponent<EntityModel>().GetCurrentLevel();
-    targetPosition = entity.GetComponent<EntityModel>().GetCurrentPosition() + direction;
-    targetPositionKey = targetPosition.x.ToString() + "," + targetPosition.y.ToString();
+    targetPosition = entity.GetComponent<EntityModel>().GetCurrentPosition() +
+      direction;
+    targetPositionKey = targetPosition.x.ToString() + "," +
+      targetPosition.y.ToString();
 
     return track[currentLevel][targetPositionKey];
   }
