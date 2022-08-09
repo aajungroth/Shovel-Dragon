b88e7cacfecaccb455f9fc5bc127ea3c4d22fa5c
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 17:22:23 -0700

    Update class name

--- Assets/Scripts/EntityAI/EntityAI.cs
+++ Assets/Scripts/EntityAI/EntityAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterAI : MonoBehaviour {
+public class EntityAI : MonoBehaviour {
   public virtual Vector2 GetMove(List<EntityModel> entityList, TrackModel trackModel, EntityModel entity) {
     return Vector2.zero;
   }
