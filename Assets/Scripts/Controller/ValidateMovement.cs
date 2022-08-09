4285eda5856082963adee18766c7d11c2a6146e5
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 18:53:08 -0700

    Update Entity type to GameObject

--- Assets/Scripts/Controller/ValidateMovement.cs
+++ Assets/Scripts/Controller/ValidateMovement.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class ValidateMovement : MonoBehaviour {
   // Determines if the entity's attempted move will result in the
   // entity moving to an empty space
-  public static bool IsMoveValid(Vector2 direction, TrackModel trackModel, EntityModel entity) {
+  public static bool IsMoveValid(Vector2 direction, GameObject entity, TrackModel trackModel) {
     List<IDictionary<string, bool>> track = trackModel.GetTrack();
     int currentLevel = 0;
     Vector2 targetPosition = Vector2.zero;
     string targetPositionKey = "";
 
-    currentLevel = entity.GetCurrentLevel();
-    targetPosition = entity.GetCurrentPosition() + move;
+    currentLevel = entity.GetComponent<EntityModel>().GetCurrentLevel();
+    targetPosition = entity.GetComponent<EntityModel>().GetCurrentPosition() + move;
     targetPositionKey = targetPosition.x.ToString() + "," + targetPosition.y.ToString();
 
     return track[currentLevel][targetPositionKey];
