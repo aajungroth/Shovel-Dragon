7e9c3f49c48ac58290d9782f146e0c73f3e07a4e
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 21:27:48 -0700

    Comment out Emote Monster until a crab is ready and reorder the variables so singles come before lists

--- Assets/Scripts/Model/RoleManager/TutorialRoleManager.cs
+++ Assets/Scripts/Model/RoleManager/TutorialRoleManager.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class TutorialRoleManager : RoleManager {
   // The references to the GameObject entities that will be assigned the roles
+  // public EntityModel emoteMonster;
+
   public List<EntityModel> doorList;
   public List<EntityModel> keyList;
   public List<EntityModel> mapList;
 
-  public EntityModel emoteMonster;
-
   // The names of the roles
+  // public const string emoteMonsterRole = "emote";
+
   public const string doorRole = "door";
   public const string keyRole = "key";
   public const string mapRole = "map";
 
-  public const string emoteMonsterRole = "emote";
-
   // Awake is called before any other Start method
   void Awake() {
+    // AssignRole(emoteMonster, emoteMonsterRole);
+
     AssignRoleList(doorList, doorRole);
     AssignRoleList(keyList, keyRole);
     AssignRoleList(mapList, mapRole);
-
-    AssignRole(emoteMonster, emoteMonsterRole);
   }
 }
