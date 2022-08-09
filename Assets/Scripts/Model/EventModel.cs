9a5a65e69e5718dbc34b8ba9aaece78e0108eea0
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 20:44:05 -0700

    Add default values

--- Assets/Scripts/Model/EventModel.cs
+++ Assets/Scripts/Model/EventModel.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EventModel : MonoBehaviour {
-  public string ability;
-  public Vector2 direction;
-  public Vector2 endPosition;
+  public string ability = "none";
+  public Vector2 direction = Vector2.zero;
+  public Vector2 endPosition = Vector2.zero;
   public GameObject entity;
-  public Vector2 startPosition;
+  public Vector2 startPosition = Vector2.zero;
 }
