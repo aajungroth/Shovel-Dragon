f5a27114ce15707561400bdef9b86be8678b9711
aajungroth <aajungroth@ucdavis.edu>
2022-08-08 18:58:44 -0700

    Update direction type from string to Vector2

--- Assets/Scripts/Model/EventModel.cs
+++ Assets/Scripts/Model/EventModel.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EventModel : MonoBehaviour
 {
   public string ability;
-  public string direction;
+  public Vector2 direction;
   public Vector2 endPosition;
   public GameObject entity;
   public Vector2 startPosition;
