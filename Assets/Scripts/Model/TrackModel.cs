f2612e4b3259c0fd022675b1fe20abe96be88bc7
aajungroth <aajungroth@ucdavis.edu>
2022-08-11 16:50:40 -0700

    Add current level, get and set methods, and public intializer variable

--- Assets/Scripts/Model/TrackModel.cs
+++ Assets/Scripts/Model/TrackModel.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrackModel : MonoBehaviour {
+  // The level of the track that the player begins on
+  public int initialLevel = 0;
+
+  // The level of the track that the player is currently on
+  private int currentLevel = 0;
+
   // List of level text files
   public List<TextAsset> trackText;
 
@@ -14,6 +20,9 @@ public class TrackModel : MonoBehaviour {
 
   // Awake is called before any other Start method
   void Awake() {
+    // Initialize the current level
+    currentLevel = initialLevel;
+
     // Get the track reader
     trackReader = gameObject.GetComponent<TrackReader>();
 
@@ -21,8 +30,19 @@ public class TrackModel : MonoBehaviour {
     track = trackReader.readTrack(trackText);
   }
 
+  // Get the current level
+  public int GetCurrentLevel() {
+    return currentLevel;
+  }
+
   // Get the track of levels
   public List<IDictionary<string, bool>> GetTrack() {
     return track;
+  }
+
+  // Set the current level
+  public int SetCurrentLevel(int level) {
+    currentLevel = level;
+    return currentLevel;
   }
 }
