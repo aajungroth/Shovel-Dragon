5f0dec2839150ec61b1d04848bbd3823ff7c3292
aajungroth <aajungroth@ucdavis.edu>
2022-09-08 14:41:19 -0700

    Add stub methods with comments to the CanvasController

--- Assets/Scripts/Controller/CanvasController.cs
+++ Assets/Scripts/Controller/CanvasController.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CanvasController : MonoBehaviour {
+  // Disables player movement, digging, burrying, power up, and idle controls
+  public void disablePlayerControls() {
 
+  }
+
+  // Disables the standard UI and enables the level clear screen
+  public void displayLevelClear() {
+
+  }
 }
