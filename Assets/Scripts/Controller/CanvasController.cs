74e16bfcc842675dd21d76223285eba3293ce7f5
aajungroth <aajungroth@ucdavis.edu>
2022-09-08 14:35:45 -0700

    Remove unused methods and adjust spacing

--- Assets/Scripts/Controller/CanvasController.cs
+++ Assets/Scripts/Controller/CanvasController.cs
@@ -2,17 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CanvasController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CanvasController : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
