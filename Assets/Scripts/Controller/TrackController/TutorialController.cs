e201dd66c6a8f56a7bfbb9c4982ccf84d010cf2f
aajungroth <aajungroth@ucdavis.edu>
2022-09-08 12:49:50 -0700

    Add code to update the level in the track model and move the player to the next level

--- Assets/Scripts/Controller/TrackController/TutorialController.cs
+++ Assets/Scripts/Controller/TrackController/TutorialController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class TutorialController : TrackController {
   public CameraController cameraController;
+  public TrackModel trackModel;
 
   protected override void resolveDoorPlayerCollision(GameObject door, GameObject player) {
+    trackModel.SetCurrentLevel(1);
+    player.GetComponent<PlayerModel>().SetCurrentLevel(1);
+    player.GetComponent<PlayerModel>().SetCurrentPosition(
+      player.GetComponent<PlayerModel>().initialPosition);
+    player.GetComponent<PlayerModel>().SetCurrentDirection("right");
+    player.transform.position += Vector3.right * 14;
     cameraController.MoveCameraRight();
   }
 }
