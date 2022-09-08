e20ad7f781f9c8b10b711645953655f80011f2cc
aajungroth <aajungroth@ucdavis.edu>
2022-09-08 16:32:53 -0700

    Update with camera pan distance public variable

--- Assets/Scripts/Controller/TrackController/TutorialController.cs
+++ Assets/Scripts/Controller/TrackController/TutorialController.cs
@@ -7,6 +7,8 @@ public class TutorialController : TrackController {
   public CanvasController canvasController;
   public TrackModel trackModel;
 
+  public float cameraPanDistance = 19f;
+
   protected override void resolveDoorPlayerCollision(GameObject door,
   GameObject player) {
     string doorRole = door.GetComponent<DoorModel>().GetRole();
@@ -20,7 +22,7 @@ public class TutorialController : TrackController {
         player.GetComponent<PlayerModel>().initialPosition);
       player.GetComponent<PlayerModel>().SetCurrentDirection("right");
       player.transform.position += Vector3.right * 14;
-      cameraController.MoveCameraRight();
+      cameraController.MoveCameraRight(cameraPanDistance);
     }
     else if (doorRole[doorRole.Length - 1] == '2') {
       canvasController.displayLevelClear();
